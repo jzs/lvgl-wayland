16d58f0363411d452b8744666f1b878be70c4cda
Lee Lup Yuen <luppy@appkaki.com>
2020-07-17 21:09:43 +0800

    Shift vertex

--- wayland/texture.c
+++ wayland/texture.c
@@ -127,19 +127,23 @@ void Draw(ESContext *esContext)
     assert(userData != NULL);
 
     GLfloat vVertices[] = {
-        -1.0f * LV_SCALE_RES, 1.0f * LV_SCALE_RES, 0.0f,  // Position 0
+        0.0f * LV_SCALE_RES, 2.0f * LV_SCALE_RES, 0.0f,  // Position 0
+        //-1.0f * LV_SCALE_RES, 1.0f * LV_SCALE_RES, 0.0f,  // Position 0
         //-0.5f, 0.5f, 0.0f,  // Position 0
         0.0f, 0.0f,         // TexCoord 0
 
-        -1.0f * LV_SCALE_RES, -1.0f * LV_SCALE_RES, 0.0f, // Position 1
+        0.0f * LV_SCALE_RES, 0.0f * LV_SCALE_RES, 0.0f, // Position 1
+        //-1.0f * LV_SCALE_RES, -1.0f * LV_SCALE_RES, 0.0f, // Position 1
         //-0.5f, -0.5f, 0.0f, // Position 1
         0.0f, 1.0f,         // TexCoord 1
 
-        1.0f * LV_SCALE_RES, -1.0f * LV_SCALE_RES, 0.0f,  // Position 2
+        2.0f * LV_SCALE_RES, 0.0f * LV_SCALE_RES, 0.0f,  // Position 2
+        //1.0f * LV_SCALE_RES, -1.0f * LV_SCALE_RES, 0.0f,  // Position 2
         //0.5f, -0.5f, 0.0f,  // Position 2
         1.0f, 1.0f,         // TexCoord 2
-        
-        1.0f * LV_SCALE_RES, 1.0f * LV_SCALE_RES, 0.0f,   // Position 3
+
+        2.0f * LV_SCALE_RES, 2.0f * LV_SCALE_RES, 0.0f,   // Position 3
+        //1.0f * LV_SCALE_RES, 1.0f * LV_SCALE_RES, 0.0f,   // Position 3
         //0.5f, 0.5f, 0.0f,   // Position 3
         1.0f, 0.0f          // TexCoord 3
     };
